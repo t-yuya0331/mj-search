f0dc1a8bc91610722a5d30d38588dc71b31a57ba
Yuya Takahashi <ahiru.3128@gmail.com>
2023-07-14 10:49:49 +0900

    add_mail_functionality

--- backend/app/Http/Controllers/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,8 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use App\Mail\RegisterMail;
+use Illuminate\Support\Facades\Mail;
 
 class RegisterController extends Controller
 {
@@ -66,11 +68,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user =  User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'status' =>$data['status']
         ]);
+
+        // Mail function
+        // Mail::send(new RegisterMail($user));
+
+        return $user;
     }
 }
